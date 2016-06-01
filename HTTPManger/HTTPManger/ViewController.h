87a2358958651c8a4489c1dd73f8666e85a8d523
oxweiwei <student@ZXW.local>
2016-06-01 09:34:47 +0800

    oxweiwei
    add pro

---
+++ HTTPManger/HTTPManger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPManger
+//
+//  Created by student on 16/5/19.
+//  Copyright © 2016年 ZXW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HTTPManger/HTTPManger/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  HTTPManger
+//
+//  Created by student on 16/5/19.
+//  Copyright © 2016年 ZXW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+
+    NSLog(@"%@",[NSRunLoop currentRunLoop]);
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
